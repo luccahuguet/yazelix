67280b83dd880282437688b980aec2852bd4c0dd
luccahuguet <lucca.huguet2@gmail.com>
2025-02-28 00:23:49 -0300

    cleanup

--- yazi/sidebar/init.lua
+++ yazi/sidebar/init.lua
@@ -10,19 +10,13 @@ end
 
 -- Function to determine and set the layout
 local function set_layout()
-    -- Check env var first (set by script)
-    local sidebar_size = os.getenv("YAZI_SIDEBAR_SIZE")
-
-    -- If no env var, calculate size based on terminal width
-    if not sidebar_size then
-        local width = get_terminal_width()
-        if width > 120 then
-            sidebar_size = "big"
-        elseif width > 80 then
-            sidebar_size = "medium"
-        else
-            sidebar_size = "small"
-        end
+    local width = get_terminal_width()
+    if width > 120 then
+        sidebar_size = "big"
+    elseif width > 80 then
+        sidebar_size = "medium"
+    else
+        sidebar_size = "small"
     end
 
     -- Map size to layout
