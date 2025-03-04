2c5fa87cf8e3bae1cd0ac508186df883c3d9d8d7
luccahuguet <lucca.huguet2@gmail.com>
2025-03-04 20:00:02 -0300

    adds yazelix branding

--- yazi/plugins/sidebar-status.yazi/main.lua
+++ yazi/plugins/sidebar-status.yazi/main.lua
@@ -1,13 +1,14 @@
 return {
-	setup = function()
+    setup = function()
+        -- Status:children_remove(1, Status.LEFT) -- MODE
+        -- Status:children_remove(2, Status.LEFT) -- size
+        Status:children_remove(3, Status.LEFT) -- Desktop
 
-		-- Status:children_remove(1, Status.LEFT) -- MODE
-		-- Status:children_remove(2, Status.LEFT) -- size
-		Status:children_remove(3, Status.LEFT) -- Desktop
-
-  	Status:children_remove(4, Status.RIGHT) -- OWNERSHIP
-		-- Status:children_remove(5, Status.RIGHT) -- percentage
-		-- Status:children_remove(6, Status.RIGHT) -- file counter
-
-	end,
+        Status:children_remove(4, Status.RIGHT) -- OWNERSHIP
+        Status:children_add(function()
+            return ui.Span("Yazelix"):fg("yellow"):bold() -- Bold and yellow
+        end, 4, Status.RIGHT)
+        -- Status:children_remove(5, Status.RIGHT) -- percentage
+        -- Status:children_remove(6, Status.RIGHT) -- file counter
+    end,
 }
