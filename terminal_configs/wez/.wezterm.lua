1bb8b3e828d087b56161bdda81d07ad1b590588f
luccahuguet <lucca.huguet2@gmail.com>
2025-03-05 15:07:06 -0300

    simplifies config and make it more resilient

--- terminal_configs/wez/.wezterm.lua
+++ terminal_configs/wez/.wezterm.lua
@@ -8,16 +8,10 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'Abernathy'
 
--- Run Zellij directly with the yazelix session
-config.default_prog = { 'zellij', '--config-dir', '~/.config/yazelix/zellij', 'attach', '--create', 'yazelix', 'options', '--default-layout', 'yazelix' }
-
--- Alternative: Use the welcome layout directly
--- config.default_prog = { 'zellij', '-l', 'welcome', '--config-dir', '~/.config/yazelix/zellij', 'options', '--layout-dir', '~/.config/yazelix/zellij/layouts' }
-
--- Fallback: Yazelix session with nu -c (if direct fails)
--- config.default_prog = { 'nu', '-c', "zellij --config-dir ~/.config/yazelix/zellij attach --create yazelix options --default-layout yazelix" }
+-- to start with yazelix, creating a new session or attaching if a session called yazelix already exists
+config.default_prog = { 'nu', '-c', "zellij --config-dir ~/.config/yazelix/zellij attach --create yazelix_wez options --default-layout yazelix" }
  
--- Fallback: Welcome layout with nu -c (if direct fails)
+-- Alternative: Pick a layout every time
 -- config.default_prog = { 'nu', '-c', "zellij -l welcome --config-dir ~/.config/yazelix/zellij options --layout-dir ~/.config/yazelix/zellij/layouts" }
  
 config.hide_tab_bar_if_only_one_tab = true
