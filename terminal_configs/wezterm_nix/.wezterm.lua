a214df62717516e74b746a30337b634dc76b4a66
luccahuguet <lucca.huguet2@gmail.com>
2025-05-18 12:59:46 -0300

    update wezterm nix config

---
+++ terminal_configs/wezterm_nix/.wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Basic settings
+config.color_scheme = 'Abernathy'
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Start Yazelix via script
+config.default_prog = { 'bash', '-c', '~/.config/yazelix/shell_scripts/start-yazelix.sh' }
+
+-- Alternative: Test without Zellij to debug crash (uncomment to test)
+-- config.default_prog = { 'bash', '-c', 'cd ~/.config/yazelix && nix develop --command nu' }
+
+-- Use server-side decorations to avoid Wayland decoration manager issues
+config.window_decorations = "NONE"
+-- Ensure clean exit to reduce Wayland resource leaks
+config.clean_exit_codes = { 0, 1 }
+
+-- Enable debug logging to diagnose crash (commented out as stable)
+-- config.debug_key_events = true
+
+-- Return the configuration to wezterm
+return config
