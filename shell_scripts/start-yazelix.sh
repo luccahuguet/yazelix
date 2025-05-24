251ad31671f9e0f5c00ae898a91f678f52f43b9f
luccahuguet <lucca.huguet2@gmail.com>
2025-05-24 19:21:31 -0300

    starts in users home

--- shell_scripts/start-yazelix.sh
+++ shell_scripts/start-yazelix.sh
@@ -14,10 +14,13 @@ echo "Resolved HOME=$HOME"
 YAZELIX_DIR="$HOME/.config/yazelix"
 
 # Navigate to Yazelix directory
+# This is important for nix develop to find the flake.nix in the current directory
 cd "$YAZELIX_DIR" || { echo "Error: Cannot cd to $YAZELIX_DIR"; exit 1; }
 
 # Run nix develop with explicit HOME.
 # The YAZELIX_DEFAULT_SHELL variable will be set by the shellHook of the flake
 # and used by the inner zellij command.
 # We use bash -c '...' to ensure $YAZELIX_DEFAULT_SHELL is expanded after nix develop sets it.
-HOME="$HOME" nix develop --impure --command bash -c "zellij --config-dir \"$YAZELIX_DIR/zellij\" options --default-layout yazelix --default-shell \"\$YAZELIX_DEFAULT_SHELL\""
+# Added --cwd "$HOME" to the zellij command to start it in the user's home directory.
+HOME="$HOME" nix develop --impure --command bash -c \
+  "zellij --config-dir \"$YAZELIX_DIR/zellij\" options --default-cwd \"$HOME\" --default-layout yazelix --default-shell \"\$YAZELIX_DEFAULT_SHELL\""
