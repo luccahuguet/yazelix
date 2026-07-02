a7b00d62b82d0f072155f2bcf894a6ff02aa2d85
drdave-flexnet <flexnetos@de-flex.net>
2026-07-01 23:00:47 -0500

    Use local beads_rust source package

--- packaging/beads_rust.nix
+++ packaging/beads_rust.nix
@@ -1,10 +1,5 @@
 {
   pkgs,
-  src ? pkgs.fetchCrate {
-    pname = "beads_rust";
-    version = "0.2.16";
-    hash = "sha256-6QM4WLC4hQArtvB2FyAxYDl/HqCEoUO7FRu6rrAFP4c=";
-  },
   rustPlatform ? pkgs.rustPlatform,
 }:
 
@@ -12,7 +7,7 @@ rustPlatform.buildRustPackage rec {
   pname = "beads_rust";
   version = "0.2.16";
 
-  inherit src;
+  src = ../../meta/beads_rust;
 
   cargoHash = "sha256-I8R0UWt+dlG05RGdASDCBo56m2vx4wSTg/pzP9eHYGg=";
 
